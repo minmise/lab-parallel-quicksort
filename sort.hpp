362767e66c71efb57394746ca73e741881cea6ae
minmise <112490043+minmise@users.noreply.github.com>
2024-12-01 23:52:53 +0300

    Delete sort.hpp

--- sort.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef SORT_HPP_
-#define SORT_HPP_
-
-void sort(int *arr, int size, int *arr_copy_left, int *arr_copy_mid, int *arr_copy_right,
-int *arr_sums_left, int *arr_sums_mid, int *arr_sums_right, int *arr_result_left, int *arr_result_mid, int *arr_result_right);
-
-#endif
